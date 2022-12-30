f866768ad5e0231622497217193706879da3381b
samikshyalamichhane <samikshya.lamichhane111@gmail.com>
2022-12-30 17:33:55 +0545

    removed vendor123 folder

--- vendor123/thebikramlama/hbl/src/routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Route::group(['prefix' => 'hbl-payment-gateway', 'namespace' => 'Thebikramlama\Hbl\Http\Controllers', 'as' => 'hbl.'], function() {
-	Route::get('error/{type?}', 'HblController@error')->name('error');
-	Route::get('/{payment_id?}', 'HblController@index')->name('index');
-});
